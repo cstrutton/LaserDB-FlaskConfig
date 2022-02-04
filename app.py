3ef74b00d640b807eea69a473bb48412b8b4bd55
Chris Strutton <chris@rodandfly.ca>
2022-02-04 15:47:04 -0500

    getting subprocess run right

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ def restart():
     ret = subprocess.run(['sudo', 'cat', '/etc/network/interfaces.d/eth0'], capture_output=True)
     return ret.stdout
 
+@app.route('/who')
+def who():
+    ret = subprocess.run(['who'], capture_output=True)
+    return ret.stdout
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
