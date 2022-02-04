f185bf0fdf62f08e7b228aee5bafb2e992026371
Chris Strutton <chris@rodandfly.ca>
2022-02-04 15:34:19 -0500

    typo

--- app.py
+++ app.py
@@ -9,10 +9,12 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
-@app.route('/restart'):
+
+@app.route('/restart')
 def restart():
     ret = subprocess.run('cat /etc/network/interfaces.d/eth0', capture_output=True)
     return ret.stdout
+
 
 if __name__ == '__main__':
     app.run()
